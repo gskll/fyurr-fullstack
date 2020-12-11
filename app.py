98e49a28830718f2176ebc068c035ffc2934b600
Andrew <gskll@protonmail.com>
2020-12-11 17:36:05 +0100

    feat: list all artists on Artists page

--- app.py
+++ app.py
@@ -161,17 +161,7 @@ def delete_venue(venue_id):
 
 @ app.route('/artists')
 def artists():
-    # TODO: replace with real data returned from querying the database
-    data = [{
-        "id": 4,
-        "name": "Guns N Petals",
-    }, {
-        "id": 5,
-        "name": "Matt Quevedo",
-    }, {
-        "id": 6,
-        "name": "The Wild Sax Band",
-    }]
+    data = [{"id": a.id, "name": a.name} for a in Artist.query.all()]
     return render_template('pages/artists.html', artists=data)
 
 
